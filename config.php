a9dbdfc90713151fd558330c9775732098ffee06
lucashe <cqlucasho@gmail.com>
2017-05-17 16:50:56 +0800

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,10 @@
 return array(
     # 开关
     'open'              => true,
+    'sms_address'       => '短信商接口地址',
     'sms_account'       => '账号',
     'sms_pass'          => '密码',
-    'sms_sign'          => '【短信商要求的前缀或后缀】',
+    'sms_sign'          => '短信前缀或后缀',
     # 使用缓存时, 定义保存验证短信的最大时间
     'sms_expire_time'   => 600,
     # 同一验证地方, 最大发送次数
