0ec88bf42c5103be2bef391b9d7a96f993e90ac4
lucashe <cqlucasho@gmail.com>
2017-05-18 09:39:49 +0800

    Update sms_memcache.php

--- sms_memcache.php
+++ sms_memcache.php
@@ -35,7 +35,7 @@ class SmsMemcache extends ASms {
      */
     public function sendSms($params = array()) {
         # 生成hash
-        if(!isset($params['token'])) $params['token'] = $params['mobile'];
+        if(empty($params['token'])) $params['token'] = $params['mobile'];
         $hashKey = $this->_generateSmsKey(self::SMS_KEY.$params['token']);
 
         # 判断是否已经发送, 并且发送次数未超过限定最大数
